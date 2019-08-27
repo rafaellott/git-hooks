048c521b68ec9a4cd9bc83630e087a6bff964d89
Rafael Lott <rafaellott@ciandt.com>
2019-08-27 10:58:42 -0300

    Added in install chmod command to hooks files

--- tools/install.sh
+++ tools/install.sh
@@ -2,4 +2,5 @@
 
 git clone https://rafaellott_cit@bitbucket.org/rafaellott_cit/git-hook.git ~/.git-hooks/
 cd ~/.git-hooks/
+chmod a+x -R ~/.git-hooks/*
 cp git-hook-config.tmpl ~/.git-hook-config.json
